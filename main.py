13bb74a282e9c18c010d6e3b9ede735e81731013
Raynel Sánchez <raynelfss@hotmail.com>
2022-02-28 17:08:09 -0500

    Pushed some testfiles in CSS and HTML.

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__, static_url_path='', static_folder="build", template_folder
 def index():
     return render_template("index.html")
 
+@app.route('/hello')
+def hello():
+    return "Hello World!"
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
+
