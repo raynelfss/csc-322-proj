54ed9d8bc655f4de63089edecb7034f297720e54
Hussam Marzooq <hussammarzooq21@gmail.com>
2022-04-10 15:41:43 -0400

    fixed some whitespace stuff cause why not

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 from flask import Flask, render_template, request
 from api.index import apiBlueprint #imports apiBlueprint from ./api/index.py
 
-
 app = Flask(__name__, static_url_path='', static_folder="build", template_folder='build')
 
 @app.route('/')
-def index():
-    return render_template("index.html")
+def index(): return render_template("index.html")
 
 @app.route('/menu')
-def menupage():
-    return render_template("menu.html")
+def menupage(): return render_template("menu.html")
 
 @app.route('/menu/edit')
-def menueditpage():
-    return render_template("menu-staff.html")
+def menueditpage(): return render_template("menu-staff.html")
 
 # all api calls /api
 app.register_blueprint(apiBlueprint)
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+if __name__ == '__main__': app.run(debug=True, use_reloader=False)
