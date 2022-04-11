f1c14f7922a18ea382c3f47f05f0f19943550b8f
Raynel Sánchez <raynelfss@hotmail.com>
2022-04-10 22:56:07 -0400

    Implemented edit methos into Items list.

--- build/menu-staff.js
+++ build/menu-staff.js
@@ -1,18 +1,10 @@
 function addElementtoTable() {        // Function adds an element to the item bar. Uses createElement() function.
-    let itemName = document.getElementById("itemName").value        // Gets itemname from input.
-    let desc = document.getElementById("description").value         // Gets description from input.
-    let imgsrc = document.getElementById("imglink").value       // Gets image link from input (Will be changed in future).
-    let price = document.getElementById("price").value      // Gets price from input.
-    
-    
-    itemName.value =  '' 
-    desc.value = '' 
-    imgsrc.value = ''  
-    price.value = '' 
-    let data = {'name': itemName, 'description' : desc, 'img_url' : imgsrc, 'price': price}     // Gathers all the data into an array.
+    let data = parseIntoDict()
     addtoDB(data)
     closeDiag("creatediv")
 }
+
+
 
 function createElement(id, itemName = "Item-name", description = "No description", price = undefined, imageSrc = undefined ) {
     let element = document.createElement("tr")         // Creates a div element with class items.
@@ -45,7 +37,7 @@ function createElement(id, itemName = "Item-name", description = "No description
     let button1 =  document.createElement("button")
     button1.append(document.createTextNode("Edit"))
     button1.onclick = function(){
-        editItem(id)
+        editDiag(id)
     }
     action.appendChild(button1)
 
@@ -90,17 +82,31 @@ async function getfromDBInd(id) {
     return data['response']
 }
 
-function openDiag(id) {
-    let diag = document.getElementById(id)
+function openDiag(motive, buttonText, id = false) {
+    let actbutton = document.getElementById("editorcreate")
+    actbutton.innerHTML = buttonText
+
+    document.getElementById("subject").innerHTML = motive
+
+    let diag = document.getElementById('creatediv')
+    if (id) {
+        actbutton.onclick = function(){
+            editItem(id)
+        }
+    }
+    else {
+        actbutton.onclick = addElementtoTable
+    }
     diag.style.display = 'flex'
 }
 
-function closeDiag(id) {
+function closeDiag() {
     document.getElementById("itemName").value =  '' 
     document.getElementById("description").value = '' 
     document.getElementById("imglink").value = ''  
     document.getElementById("price").value = '' 
-    let diag = document.getElementById(id)
+    
+    let diag = document.getElementById('creatediv')
     diag.style.display = 'none'
 }
 
@@ -132,25 +138,56 @@ async function deleteItem(id) {
     //displayItems()
 }
 
-async function editItem(id) {
-    let diag = document.getElementById("creatediv")
-    diag.style.display = "flex"
+function editItem(id) {
+    let item = parseIntoDict()
+    sendEdittoDB(id, item)
+    closeDiag()
+}
+
+async function editDiag(id) {
+    openDiag("Edit item:", "Edit", id)
     console.log("Attempted edit", id)
     let item = await getfromDBInd(id)
     console.log(item)
-    fillTextboxes(item[1], item[3], item[2], item[4])
 
+    fillTextboxes(item[1], item[3], item[2], item[4])
 }
 
-//async function sendEdit(id)
+
+async function sendEdittoDB(id, item) {
+    const response = await fetch('/api/menu/' + String(id), {
+        method: 'PUT',
+        headers: {
+            'Accept' : 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(item),
+    })
+    const data = await response.json()
+    console.log("Edited the item with id", id, response)
+    displayItems()
+}
+
+function parseIntoDict() {        // Function adds an element to the item bar. Uses createElement() function.
+    let itemName = document.getElementById("itemName").value        // Gets itemname from input.
+    let desc = document.getElementById("description").value         // Gets description from input.
+    let imgsrc = document.getElementById("imglink").value       // Gets image link from input (Will be changed in future).
+    let price = document.getElementById("price").value      // Gets price from input.
+    
+    
+    itemName.value =  '' 
+    desc.value = '' 
+    imgsrc.value = ''  
+    price.value = '' 
+    let data = {'name': itemName, 'description' : desc, 'img_url' : imgsrc, 'price': price}     // Gathers all the data into an array.
+    return data
+}
 
 function fillTextboxes(name, description, imgsrc, price) {
     document.getElementById("itemName").value = name       // Gets itemname from input.
     document.getElementById("description").value = description       // Gets description from input.
     document.getElementById("imglink").value = imgsrc       // Gets image link from input (Will be changed in future).
     document.getElementById("price").value = price      // Gets price from input.
-
-
 }
 
 function eraseElements() {
