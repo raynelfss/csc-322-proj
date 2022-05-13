172823b3a28f9f36c9f9ddc9fc9181f0a19c0ab8
Crystal Yang <crystalyang@Crystals-Air.home>
2022-05-13 15:39:17 -0400

    fixed styles for progress bar

--- build/scripts/dashboard-chefprogress-order.js
+++ build/scripts/dashboard-chefprogress-order.js
@@ -87,3 +87,40 @@ function loadStatus(status) {
 }
 
 loadOrder()
+
+const  previousBtn  =  document.getElementById('previousBtn');
+const  nextBtn  =  document.getElementById('nextBtn');
+// const  finishBtn  =  document.getElementById('finishBtn');
+const  content  =  document.getElementById('content');
+const  bullets  =  [...document.querySelectorAll('.circle')];
+
+const MAX_STEPS = 3;
+let currentStep = 1;
+
+nextBtn.addEventListener('click',  ()  =>  {
+	bullets[currentStep  -  1].classList.add('completed');
+	currentStep  +=  1;
+	previousBtn.disabled  =  false;
+	if  (currentStep  ===  MAX_STEPS)  {
+		nextBtn.disabled  =  true;
+		// finishBtn.disabled  =  false;
+	}
+	content.innerText  =  `Step ${currentStep}`;
+});
+
+
+previousBtn.addEventListener('click',  ()  =>  {
+	bullets[currentStep  -  2].classList.remove('completed');
+	currentStep  -=  1;
+	nextBtn.disabled  =  false;
+	// finishBtn.disabled  =  true;
+	if  (currentStep  ===  1)  {
+		previousBtn.disabled  =  true;
+	}
+	content.innerText  =  `Step ${currentStep}`;
+});
+
+// finishBtn.addEventListener('click',  ()  =>  {
+// 	location.reload();
+// });
+// when done with order, page will reload
