8222b70374bbed31c0376bc797e3cbd17f3afa78
Crystal Yang <crystalyang@Crystals-Air.home>
2022-05-13 15:20:22 -0400

    added progress bar to chef progress order from dashboard

--- build/scripts/updateprogress.js
+++ build/scripts/updateprogress.js
@@ -1,3 +1,27 @@
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = documnet.querySelectorAll(".circles");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+    currentActive++;
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+    update();
+});
+
+prev.addEventListener("click", () => {
+    currentActive--;
+
+    if (currentActive < 1){
+        currentActive = 1;
+    }
+    update();
+});
+
 function update(){
     circles.forEach((circle, idx) => {
         if (idx < currenActive) {
